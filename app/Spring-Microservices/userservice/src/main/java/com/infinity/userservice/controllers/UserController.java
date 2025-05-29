bc1e52499e65619d5b6c7ae4e1ad5c5326fcf8f1
Alex Argatoff <alexargatoff@gmail.com>
2025-05-29 15:10:58 -0700

    fixed up weird pom errors for user service

--- app/Spring-Microservices/userservice/src/main/java/com/infinity/userservice/controllers/UserController.java
+++ app/Spring-Microservices/userservice/src/main/java/com/infinity/userservice/controllers/UserController.java
@@ -1,5 +1,15 @@
 package com.infinity.userservice.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @GetMapping("/test")
+    public String apiTest() {
+        return "Test";
+    }
 }
