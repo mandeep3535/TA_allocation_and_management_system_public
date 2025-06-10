1ef422bded218d5ff789a55846fbbf1c5d847ea9
Seiya Iwama <90356388+sei-wiese@users.noreply.github.com>
2025-06-09 22:47:04 -0700

    feat: Change DeptCode filter to dropdown

--- app/TA-portal-infinity/src/components/features/courses/CourseFilter.tsx
+++ app/TA-portal-infinity/src/components/features/courses/CourseFilter.tsx
@@ -35,17 +35,23 @@ export default function CourseFilter({ onFilterChange }: CourseFilterProps) {
           className="border p-2 rounded-md w-full"
         >
           <option value="">All Terms</option>
-          {/* Add other terms as needed */}
+          {/* Add other terms later OR Fetch from database */}
           <option value="Winter 2025">Winter 2025</option>
           <option value="Fall 2024">Fall 2024</option>
         </select>
-        <input
-          type="text"
-          placeholder="Department Code (e.g., COSC)"
+        
+        <select
           value={deptCode}
           onChange={(e) => setDeptCode(e.target.value)}
           className="border p-2 rounded-md w-full"
-        />
+        >
+          <option value="">All Departments</option>
+          <option value="COSC">COSC</option>
+          <option value="MATH">MATH</option>
+          <option value="PHYS">PHYS</option>
+          {/* Add other department codes later OR Fetch from database */}
+        </select>
+
         <input
           type="text"
           placeholder="Course Number (e.g., 101)"
