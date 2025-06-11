46feddd53219ae37fd253fc665b6cba7390995ef
Alex Argatoff <alexargatoff@gmail.com>
2025-06-10 20:57:26 -0700

    got the spring initializr and docker files and properties set up

--- init/init.sql
+++ init/init.sql
@@ -2,10 +2,14 @@ CREATE DATABASE IF NOT EXISTS user_db;
 
 CREATE DATABASE IF NOT EXISTS course_db;
 
+CREATE DATABASE IF NOT EXISTS application_db
+
 CREATE USER 'dev_user' IDENTIFIED BY 'secret';
 
 GRANT ALL PRIVILEGES ON user_db.* TO 'dev_user';
 
 GRANT ALL PRIVILEGES ON course_db.* TO 'dev_user';
+
+GRANT ALL PRIVILEGES ON application_db.* TO 'dev_user';
 
 FLUSH PRIVILEGES;
