6c8d1cc6452e5a2f9f28032c11f74710cc5683e1
Alex Argatoff <alexargatoff@gmail.com>
2025-06-16 17:18:40 -0700

    removed uneeded imports

--- app/Spring-Microservices/applicationservice/src/main/java/com/infinity/applicationservice/dtos/ApplicationRequest.java
+++ app/Spring-Microservices/applicationservice/src/main/java/com/infinity/applicationservice/dtos/ApplicationRequest.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 import com.infinity.applicationservice.enums.Subject;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public record ApplicationRequest(
-        List<Subject> preferences,
-        @NotBlank(message = "Remote work preference not specified") 
-        boolean wantRemote,
-        @NotBlank(message = "Preferred number of work hours not specified") 
-        Integer wantWorkingHours
-
-) {
+                List<Subject> preferences,
+                @NotNull(message = "Remote work preference not specified") 
+                boolean wantRemote,
+                @NotNull(message = "Working hour preferences not specified")
+                @Min(value = 2, message = "The value must be greater than or equal to 2")
+                @Max(value = 12, message = "The value must be less than or equal to 12")
+                Integer wantWorkingHours) {
 }
