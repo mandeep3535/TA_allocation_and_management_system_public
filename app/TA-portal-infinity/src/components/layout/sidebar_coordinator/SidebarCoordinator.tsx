631b9136665b756a573e55d44d30b563f5156684
Mandeep <meritmandeep35@gmail.com>
2025-06-23 02:26:43 -0700

    minor changes in side nav

--- app/TA-portal-infinity/src/components/layout/sidebar_coordinator/SidebarCoordinator.tsx
+++ app/TA-portal-infinity/src/components/layout/sidebar_coordinator/SidebarCoordinator.tsx
@@ -53,10 +53,10 @@ export default function SideNavCoordinator() {
       </div>
 
       {/* Logout Button */}
-      <div className="border-t border-white/20 w-full px-3 py-4">
+      <div className="border-t border-white/20 w-full px-8 py-4">
         <button
           onClick={logout}
-          className="flex items-center gap-3 text-sm hover:text-red-300 transition"
+          className="flex items-center gap-5 text-sm text-white hover:text-[#a0ffe6] hover:scale-105 hover:drop-shadow-[0_0_10px_#a0ffe6] transition-all duration-300"
         >
           <LogOut size={22} />
           {expanded && <span>Logout</span>}
