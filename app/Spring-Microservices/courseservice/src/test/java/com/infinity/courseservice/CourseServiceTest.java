4dc374c327a8fbec0836356e94ee2dc204b7dae2
eeasuper <eeasuper@gmail.com>
2025-06-30 14:35:31 -0700

    merged develop

--- app/Spring-Microservices/courseservice/src/test/java/com/infinity/courseservice/CourseServiceTest.java
+++ app/Spring-Microservices/courseservice/src/test/java/com/infinity/courseservice/CourseServiceTest.java
@@ -164,7 +164,7 @@ public class CourseServiceTest {
 
         Course course = new Course("COSC", "Networks", "329");
         course.setId(courseId);
-        Section section = new Section(2025, "W1", "001", SectionType.LAB, course);
+        Section section = new Section(2025, "W1", "001", SectionType.LABORATORY, course);
         section.setId(1L);
 
         NeedDto need = new NeedDto(5L, courseId, "Grading", 30, 15, year, semester);
