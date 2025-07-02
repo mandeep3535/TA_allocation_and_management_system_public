cedc774521127a898ad12a5eb8bdcdaa0fd28233
Alex Argatoff <alexargatoff@gmail.com>
2025-07-02 11:50:27 -0700

    removed uneeded dto

--- app/Spring-Microservices/courseservice/src/main/java/com/infinity/courseservice/dtos/CourseDtos/PrereqRequest.java
+++
@@ -1,6 +0,0 @@
-package com.infinity.courseservice.dtos.CourseDtos;
-
-import java.util.List;
-
-public record PrereqRequest(Long courseId, 
-        List<Long> prereqId, Integer year, String semester) {}
