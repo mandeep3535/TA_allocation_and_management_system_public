8f9ad038121fbd38774a0938dbcc333a8e0c0691
Mandeep Singh <155117344+mandeep3535@users.noreply.github.com>
2025-07-09 21:43:39 -0700

    Update ApplicationPage.tsx
    Add another error message when foreign contraint

--- app/TA-portal-infinity/src/pages/student/applicationpage/ApplicationPage.tsx
+++ app/TA-portal-infinity/src/pages/student/applicationpage/ApplicationPage.tsx
@@ -322,7 +322,9 @@ useEffect(() => {
                               } catch (err: any) {
                                 if (err.message && err.message.includes('403')) {
                                   toast.error('You do not have permission to delete this application.', { autoClose: 3500 });
-                                } else {
+                                }  else if (errorMessage.includes('Failed to delete application')) {
+                                  toast.error('This application has allocations and cannot be deleted.', { autoClose: 3500 });
+                                }  else {
                                   toast.error('Failed to delete application. Please try again.', { autoClose: 3500 });
                                 }
                                 console.error(err);
