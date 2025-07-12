e03f88e23bcde8153dc7e78355dcaf5eb0d6f0d2
eeasuper <eeasuper@gmail.com>
2025-07-12 15:54:47 -0700

    minor error fix

--- app/Spring-Microservices/userservice/src/test/java/com/infinity/userservice/AuthServiceTest.java
+++ app/Spring-Microservices/userservice/src/test/java/com/infinity/userservice/AuthServiceTest.java
@@ -13,6 +13,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
