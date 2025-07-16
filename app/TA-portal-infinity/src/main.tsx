0cdb98ec22922903318c3f0223f362548b96e7a1
eeasuper <eeasuper@gmail.com>
2025-07-16 13:51:45 -0700

    removed tanstack devtools

--- app/TA-portal-infinity/src/main.tsx
+++ app/TA-portal-infinity/src/main.tsx
@@ -5,7 +5,6 @@ import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.tsx';
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -15,7 +14,6 @@ createRoot(document.getElementById('root')!).render(
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>  
   </AuthProvider>
   </StrictMode >,
