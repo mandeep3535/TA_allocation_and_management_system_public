2946af548d039c6ae5c8d6c5af9ef3d4dbd4e1d1
Aakash Singh <aakashds@student.ubc.ca>
2025-07-18 09:55:32 -0700

    uninstalled axios

--- app/TA-portal-infinity/src/pages/coordinator/exampage/CreateExamPage.tsx
+++ app/TA-portal-infinity/src/pages/coordinator/exampage/CreateExamPage.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useNavigate } from 'react-router-dom';
