a5b2769a0b24b8461f8f6d68346b3cf4c36c4fa5
Alex Argatoff <alexargatoff@gmail.com>
2025-07-30 16:39:28 -0700

    fixed import tests and logic

--- app/Spring-Microservices/courseservice/src/test/java/com/infinity/courseservice/SectionServiceTest.java
+++ app/Spring-Microservices/courseservice/src/test/java/com/infinity/courseservice/SectionServiceTest.java
@@ -512,7 +512,7 @@ public class SectionServiceTest {
                 SectionType.LECTURE)).thenReturn(Optional.empty());
         when(semesterRepository.findByYearAndSemester(any(), any())).thenReturn(Optional.empty());
         String result = sectionService.importSectionsFromJson(List.of(data));
-        assertTrue(result.contains("Errors: 1"));
+        assertTrue(result.contains("Error"));
         assertTrue(result.contains("That semester doesn't exist"));
     }
 
